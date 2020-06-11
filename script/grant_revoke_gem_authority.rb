d3ec7893155a7ff4392ab51667ef78d101be5dfe
Bess Sadler <bess@curationexperts.com>
2020-06-11 10:09:21 -0400

    Adding Rachel Kadel and Randall Floyd
    Both for component maintenance work

--- script/grant_revoke_gem_authority.rb
+++ script/grant_revoke_gem_authority.rb
@@ -58,7 +58,9 @@ KNOWN_COMMITTER_EMAIL_ADDRESSES = {
   'tpendragon' => "tpendragon@princeton.edu",
   'carrickr' => 'carrickr@umich.edu',
   'no_reply' => 'johnson.tom@gmail.com',
-  'revgum' => 'revgum@gmail.com'
+  'revgum' => 'revgum@gmail.com',
+  'rmkadel' => 'rachelkg@gmail.com',
+  'randalldfloyd' => 'randalldfloyd@gmail.com'
 }
 # Some GitHub repositories are named differently from their gems
 KNOWN_MISMATCHED_GEM_NAMES = {
