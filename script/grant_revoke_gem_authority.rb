1f695968b20ae4639d5bf9fd405bf2a538f7d995
Bess Sadler <bess@curationexperts.com>
2020-06-11 10:05:32 -0400

    Jamie Little is no longer working on samvera

--- script/grant_revoke_gem_authority.rb
+++ script/grant_revoke_gem_authority.rb
@@ -57,7 +57,6 @@ KNOWN_COMMITTER_EMAIL_ADDRESSES = {
   'ndushay' => "ndushay@stanford.edu",
   'tpendragon' => "tpendragon@princeton.edu",
   'carrickr' => 'carrickr@umich.edu',
-  'little9' => 'jamielittle@outlook.com',
   'no_reply' => 'johnson.tom@gmail.com',
   'revgum' => 'revgum@gmail.com',
   'geekscruff' => 'julie.allinson@london.ac.uk'
